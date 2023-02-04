608878a72d32d39ec9177f822ffa6a5b7a1e9acd
Dorian <dorianpernot2@gmail.com>
2023-02-04 01:54:33 +0100

    MAJ JS début regexp ET quelques MAJ CSS

--- modal.js
+++ modal.js
@@ -14,7 +14,6 @@ const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
 const modalbgContent = document.querySelector(".content");
 
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -28,30 +27,29 @@ closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // close modal form
 function closeModal() {
-
   // Ajout de la class "content-off" afin de déclencher l'animation de fermeture du modal d'inscription
   modalbgContent.classList.add("content-off");
-  setTimeout(function() {
+  setTimeout(function () {
     modalbg.style.display = "none";
   }, 450);
-  setTimeout(function() {
+  setTimeout(function () {
     modalbgContent.classList.remove("content-off");
   }, 500);
 }
 
 // delete active class when we are in home page
-const links = document.querySelectorAll('.nav-link');
-    
+const links = document.querySelectorAll(".nav-link");
+
 if (links.length) {
   links.forEach((link) => {
-    link.addEventListener('click', (e) => {
+    link.addEventListener("click", (e) => {
       links.forEach((link) => {
-          link.classList.remove('active');
-          link.style.display = "block";
+        link.classList.remove("active");
+        link.style.display = "block";
       });
       e.preventDefault();
-      link.classList.add('active');
-      const spans = link.querySelectorAll('a.nav-link span');
+      link.classList.add("active");
+      const spans = link.querySelectorAll("a.nav-link span");
       spans.forEach((span) => {
         if (span.textContent === "Accueil") {
           link.style.display = "none";
@@ -61,18 +59,82 @@ if (links.length) {
   });
 }
 
-// Quand le formulaire complet est envoyé, msg de confirmation
+// Vérification de la validité des différents champs du formulaire et soumission de msgs d'erreur dans le cas contraire
 function validate() {
 
-  const first = document.forms["reserve"]["first"].value;
-  const last = document.forms["reserve"]["last"].value;
-  const email = document.forms["reserve"]["email"].value;
-  const birthdate = document.forms["reserve"]["birthdate"].value;
-  const quantity = document.forms["reserve"]["quantity"].value;
+  // vérifie si le prénom et le nom ont des valeurs correctes (que du texte)
+  function checkFirstLast() {
+    let first = document.forms["reserve"]["first"].value;
+    let last = document.forms["reserve"]["last"].value;
+    let regexpCheckFirstLast = /^[a-zA-Z\u00C0-\u00FF-]+$/;
+    if (regexpCheckFirstLast.test(first) && regexpCheckFirstLast.test(last)) {
+      console.log("Les valeurs des inputs sont valides");
+    } else {
+      console.log("Les valeurs des inputs ne sont pas valides");
+    }
+  }
 
-  const checkbox1 = document.getElementById("checkbox1").value;
-  const checkbox2 = document.getElementById("checkbox2").value;
+  // Vérifie si l'adresse email est valide
+  function isValidEmail() {
+    let email = document.forms["reserve"]["email"].value;
 
+    let emailRegexp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailRegexp.test(email)) {
+      console.log("L'email est valide");
+    } else {
+      console.log("L'email n'est pas valide");
+    }
+  }
+
+  // Vérifie que la personne est majeure
+  function checkAge() {
+    let birthdate = new Date(document.forms["reserve"]["birthdate"].value);
+    let today = new Date();
+    let age = today.getFullYear() - birthdate.getFullYear();
+    let m = today.getMonth() - birthdate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
+      age--;
+    }
+    if (age >= 18) {
+      console.log("La personne est majeure.");
+    } else {
+      console.log("La personne n'est pas majeure.");
+    }
+  }
+
+  // Vérifie qu'un chiffre/nombre entre 0 et 99 est entrée
+  let quantity = document.forms["reserve"]["quantity"].value;
+  if (quantity >= 0 && quantity <= 99 && !isNaN(quantity)) {
+    console.log("La valeur entrée est valide.");
+  } else {
+    console.log("La valeur entrée n'est pas valide.");
+  }
+
+  // Vérifie qu'au moins un input de type "radio" est sélectionné
+  function checkRadioSelection() {
+    let radioInputs = document.querySelectorAll('input[type="radio"]');
+    for (let i = 0; i < radioInputs.length; i++) {
+      if (radioInputs[i].checked) {
+        return true;
+      }
+    }
+    return false;
+  }
+  if (checkRadioSelection()) {
+    console.log("Au moins un bouton radio est sélectionné");
+  } else {
+    console.log("Aucun bouton radio n'est sélectionné");
+  }
+
+  // Vérifie que les terms sont acceptés
+  let checkbox = document.getElementById("checkbox1");
+  if (checkbox.checked) {
+    console.log("La case à cocher est cochée.");
+  } else {
+    console.log("La case à cocher n'est pas cochée.");
+  }
 
   // Si tout est valide, soumission
   alert("Merci ! Votre réservation a été reçue.");
@@ -80,4 +142,3 @@ function validate() {
 }
 
 // Gestion des erreures dans le formumaire
-
